4a2236184f41e9af5254059603b44d05690ef307
Thomas Donnelly <Thomas.W.Donnelly@gmail.com>
2020-12-20 22:29:34 -0600

    added signale logger

---
+++ you-gonna-learn-today-app/src/utils/logger.js
@@ -0,0 +1,14 @@
+const { Signale } = require("signale");
+
+const signale = new Signale();
+signale.error("Default Error Log");
+signale.success("Default Success Log");
+signale.remind("Improve documentation");
+
+const options = {};
+const custom = new Signale(options);
+custom.error("Custom Error log");
+custom.success("Custom Success log");
+custom.remind("Improve documentation");
+
+module.exports = { signale, custom };
