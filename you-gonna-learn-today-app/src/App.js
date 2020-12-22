b415fa4d48460036833dcbd9e8a3ddc7236f501b
Thomas Donnelly <Thomas.W.Donnelly@gmail.com>
2020-12-21 18:14:08 -0600

    added axios for api call to opentdb.com for sample question data

--- you-gonna-learn-today-app/src/App.js
+++ you-gonna-learn-today-app/src/App.js
@@ -1,10 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import logo from "./img/logo.svg";
 import "./styles/App.css";
+import axios from "axios";
 import FlashcardList from "./components/FlashcardList";
 
 function App() {
   const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
+
+  useEffect(() => {
+    // using https://opentdb.com for initial data
+    axios.get("https://opentdb.com/api.php?amount=10").then((res) => {
+      setFlashcards(
+        res.data.results.map((questionItem, index) => {
+          const answer = decodeString(questionItem.correct_answer);
+          const options = [
+            ...questionItem.incorrect_answers.map((a) => decodeString(a)),
+            answer,
+          ];
+          return {
+            id: `${index}-${Date.now()}`,
+            question: decodeString(questionItem.question),
+            answer: answer,
+            options: options.sort(() => Math.random() - 0.5),
+          };
+        })
+      );
+    });
+  }, []);
+
+  function decodeString(str) {
+    const textArea = document.createElement("textarea");
+    textArea.innerHTML = str;
+    return textArea.value;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
